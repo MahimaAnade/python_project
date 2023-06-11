9f6ebc58a3a24d63781bb5a56a0069b0e7cf4c91
MahimaAnade <134799916+MahimaAnade@users.noreply.github.com>
2023-06-11 15:44:14 +0530

    01_python_ass.py

--- python_ass.py
+++ python_ass.py
@@ -1,11 +1,12 @@
+#python minor project on countdown timer by Mahima Anade CSE(A)
 import time
 
 def countdown_timer(seconds):
     while seconds > 0:
         print(seconds)
-        time.sleep(1)  # Wait for 1 second
+        time.sleep(1) 
         seconds -= 1
     print("Time's up!")
 
-# Example usage: countdown for 10 seconds
+
 countdown_timer(10)
